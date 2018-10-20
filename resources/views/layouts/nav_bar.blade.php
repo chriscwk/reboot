c3a56403c84f678dfad4eef95b13a092114bf39a
Christopher Chan <christopher.ccwk@gmail.com>
2018-10-20 11:30:44 +0800

    Idk

--- resources/views/layouts/nav_bar.blade.php
+++ resources/views/layouts/nav_bar.blade.php
@@ -13,9 +13,9 @@
 			<ul>
 				<li><a id="home" href="javascript:;">Home</a></li>
 				<li><a id="categories" href="javascript:;">Categories</a></li>
-				<li><a id="aboutUs" href="javascript:;">About Us</a></li>
+				{{-- <li><a id="aboutUs" href="javascript:;">About Us</a></li> --}}
+				<li><a id="login" href="javascript:;">Sign In</a></li>
 				<li><a href="javascript:;">Sign Up</a></li>
-				<li><a id="login" href="javascript:;">Log In</a></li>
 			</ul>
 		</div>
 	</nav>
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,53 +4,38 @@
 
 @section('content')
 
-	<div class="bg-first"></div>
-	<div class="bg-second"></div>
-	<div class="bg-third"></div>
+	<div class="main-container">
 
-	<div class="top-content">
-
-	</div>
-	<div class="categories-content">
-
-	</div>
-	<div class="all-content">
-		
 	</div>
 
 	<div class="page-footer">
-		<div class="su-container hidden">
-			<i class="fas fa-angle-double-up"></i>
-			<div>scroll up</div>
-		</div>
-		<div class="sd-container">
-			<i class="fas fa-angle-double-down"></i>
-			<div>scroll down</div>
-		</div>
+		
 	</div>
 
 	<div class="login-overlay">
 		<div class="login-container">
-			<div class="login-header">LOG IN<span id="close_login" class="pull-right"><i class="fas fa-times"></i></span></div>
+			<div class="login-header">SIGN IN<span id="close_login" class="pull-right"><i class="fas fa-times"></i></span></div>
 			<form method="POST" action="{{ action('LoginController@login') }}">
 				@csrf
 
 				<div class="login-body">
-					<div class="row">
-						<div class="col-xl-12">
-							<div class="form-group">
-								<label>Email</label>
-								<input type="text" class="form-control" name="user_email" placeholder="Enter Email" required />
+					<div class="login-email">
+						<div class="row">
+							<div class="col-xl-12">
+								<div class="form-group">
+									<label>Email</label>
+									<input type="text" class="form-control" name="user_email" placeholder="Enter Email" required />
+								</div>
 							</div>
-						</div>
-						<div class="col-xl-12">
-							<div class="form-group">
-								<label>Password</label>
-								<input type="password" class="form-control" name="user_pass" placeholder="Enter Password" required />
+							<div class="col-xl-12">
+								<div class="form-group">
+									<label>Password</label>
+									<input type="password" class="form-control" name="user_pass" placeholder="Enter Password" required />
+								</div>
 							</div>
-						</div>
-						<div class="col-xl-12 text-center">
-							<button type="submit" class="btn btn-outline-secondary m-t-20">Log In</button>
+							<div class="col-xl-12 text-center">
+								<button type="submit" class="btn btn-outline-secondary m-t-20">SIGN IN</button>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -64,56 +49,6 @@
 	<script>
 
 	    $(function() {
-	    	var currDisplay = 'first';
-
-	    	$('body').on('mousewheel DOMMouseScroll', function(e){
-	    		e.preventDefault();
-
-				if(typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0) {
-				    if(e.originalEvent.detail > 0) {
-				      	scrollDown();
-				    } else if(e.originalEvent.detail < 0){
-				        scrollUp();
-				    }
-				} else if (typeof e.originalEvent.wheelDelta == 'number') {
-				    if(e.originalEvent.wheelDelta < 0) {
-				    	scrollDown();
-				    } else if(e.originalEvent.wheelDelta > 0) {
-				    	scrollUp();
-				    }
-				}
-			});
-
-			$('#home').on('click', function() {
-				$('nav, .page-footer').fadeOut(10, function() {
-	    			scrollTo('.bg-first');
-	    			currDisplay = 'first';
-
-	    			$('.su-container').addClass('hidden');
-	    			$('.sd-container').removeClass('hidden');
-	    		});
-			});
-
-			$('#categories').on('click', function() {
-				$('nav, .page-footer').fadeOut(10, function() {
-					scrollTo('.bg-second');
-	    			currDisplay = 'second';
-
-	    			$('.su-container').removeClass('hidden');
-	    			$('.sd-container').removeClass('hidden');
-				});
-			});
-
-			$('#aboutUs').on('click', function() {
-				$('nav, .page-footer').fadeOut(10, function() {
-	    			scrollTo('.bg-third');
-	    			currDisplay = 'third';
-
-	    			$('.su-container').removeClass('hidden');
-	    			$('.sd-container').addClass('hidden');
-	    		});
-			});
-
 			$('#login').on('click', function() {
 				$('.login-overlay').addClass('show');
 				$('.login-container').delay( 500 ).fadeIn();
@@ -124,53 +59,6 @@
 					$('.login-overlay').removeClass('show');
 				});
 			});
-
-			function scrollTo(position) {
-				$('nav, .page-footer').addClass('fixed-nav');
-		    	$('.top-content').css('margin-top', '7.5vh');
-		    	
-				$('html, body').animate({
-                    scrollTop: $(position).offset().top
-                }, 500, function() {
-                	$('nav, .page-footer').fadeIn();
-                });
-			}
-
-			function scrollDown() {
-				if(currDisplay == 'first') {
-					$('nav, .page-footer').fadeOut(10, function() {
-						scrollTo('.bg-second');
-		    			currDisplay = 'second';
-
-		    			$('.su-container').removeClass('hidden');
-					});
-		    	} else if(currDisplay == 'second') {
-		    		$('nav, .page-footer').fadeOut(10, function() {
-		    			scrollTo('.bg-third');
-		    			currDisplay = 'third';
-
-		    			$('.sd-container').addClass('hidden');
-		    		});
-		    	}
-			}
-
-			function scrollUp() {
-				if(currDisplay == 'third') {
-					$('nav, .page-footer').fadeOut(10, function() {
-						scrollTo('.bg-second');
-		    			currDisplay = 'second';
-
-		    			$('.sd-container').removeClass('hidden');
-					});
-		    	} else if(currDisplay == 'second') {
-		    		$('nav, .page-footer').fadeOut(10, function() {
-		    			scrollTo('.bg-first');
-		    			currDisplay = 'first';
-
-		    			$('.su-container').addClass('hidden');
-		    		});
-		    	}
-			}
 	    });
 	</script>
 @endsection
